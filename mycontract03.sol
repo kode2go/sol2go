e3c24471876601beb184ed8630b286ae5a8a5640
kode2go <brsbin001@myuct.ac.za>
2022-05-17 18:08:46 +0200

    Update mycontract03.sol

--- mycontract03.sol
+++ mycontract03.sol
@@ -1,11 +1,37 @@
 // SPDX-License-Identifier: GPL-3.0
-// ref: https://ethereum-blockchain-developer.com/010-solidity-basics/05-string-types/
 pragma solidity ^0.8.7;
 
-contract StringExample {
-    string public myString = 'hello world!';
+contract MyContract {
 
-    function setMyString(string memory _myString) public {
-        myString = _myString;
+    // create a state variable called mystring
+    //...that is visible on the blockchain
+    //...and is initialized with "empty"
+
+    string public mystring = "empty";
+
+    uint public count;
+
+    function setMyString(string memory _mystring) public {
+        mystring = _mystring;
     }
+
+    //two functions to increment and decrement the count store in this contract.
+
+    // function to get the current count
+    function get() public view returns (uint)
+    {
+        return count;
+    }
+
+    // function to increment the count by 1
+    function inc() public {
+        count = count + 1;
+    }
+
+    // function to decrement the count by 1
+    function dec() public {
+        // This function will fail if count = 0
+        count -= 1;
+    }
+
 }
